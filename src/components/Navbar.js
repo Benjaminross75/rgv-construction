80234b1abeff18c83696aaaa4bc95778a5e322e1
Ben Ross <mrbenross@gmail.com>
2024-04-01 09:32:00 -0500

    Home.css updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../assets/logo1.jpg';
+import '../styles/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
+
+
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleNavbar = () =>{
+  setToggle(!toggle)
+  }
+
   return (
     <div className='navbar'>
-      <div className='leftSide'>
+      <div className='leftSide' id={toggle ? "open" : "close"}>
         <img src={logo} alt={"website logo"}/>
+        <div className='hiddenLinks'>
+        <Link to="/"> Home </Link>
+        <Link to="/services"> Service </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        </div>
       </div>
       <div className='rightSide'>
         <Link to="/"> Home </Link>
         <Link to="/services"> Service </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon/>
+        </button>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import {Link}  from 'react-router-dom';
+import bannerImage from '../assets/home-porch.jpg';
+import '../styles/Home.css'
 function Home() {
   return (
-    <div>
-      Hello World!
+    <div className='home'>
+      <div className='headerContainer' style={{backgroundImage: `url(${bannerImage})` }}>
+        <h1>RGV Construction</h1>
+        <p>Building premium decks and fences</p>
+        <Link to="/services">
+        <button>Our services</button>
+        </Link>
+      </div>
     </div>
   );
 }

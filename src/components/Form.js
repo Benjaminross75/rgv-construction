6c52161a206d85fba39cb632ceae2f7fd5b783af
Ben Ross <mrbenross@gmail.com>
2024-04-03 10:29:26 -0500

    form error handling created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import {yupResolver} from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import '../styles/Form.css';
 const Form = () => {
     const schema = yup.object().shape({
         name: yup.string()
-        .required()
+        .required("Your name is required")
         .trim()
         .min(2, "Name must be at least 2 characters")
         .max(50, "Name cannot exceed 50 characters")
@@ -16,16 +17,16 @@ const Form = () => {
         }),
         email: yup.string()
         .email()
-        .required(),
+        .required("Your email is required"),
         message: yup.string()
-        .required()
+        .required("A message is required")
         .min(10, "Message must be a least 10 characters")
         .max(200, "Message cannot exceed 200 characters")
 
     });
 
-    const {register, handleSubmit} = useForm({
-        resolver:
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema),
     });
 
 
@@ -37,8 +38,10 @@ const Form = () => {
     <form onSubmit={handleSubmit(onSubmit)} id="contact-form" method='POST'>
     <label htmlFor='name'>Full Name</label>
     <input name="name" placeholder='Enter Full Name...' type='text' {...register("name")}/>
+    {errors.name && <p>{errors.name?.message}</p>}
     <label htmlFor='email'>Email</label>
     <input name="email" placeholder='Enter email...' type='email' {...register("email")}/>
+    {errors.email && <p>{errors.email?.message}</p>}
     <label htmlFor='message'>Message</label>
     <textarea rows='6'
               placeholder='Enter message...'
@@ -47,6 +50,7 @@ const Form = () => {
               {...register("message")}
               >
      </textarea>
+     {errors.message && <p>{errors.message?.message}</p>}
      <button type='submit'>Send Message</button>
  </form>
   );
